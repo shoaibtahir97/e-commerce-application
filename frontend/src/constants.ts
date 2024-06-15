3cba1a8875e490eb52ad2e23e3e6c9d8f857d90c
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-06-16 00:40:18 +0500

    created constants files

---
+++ frontend/src/constants.ts
@@ -0,0 +1,5 @@
+const BASE_URL = "http://localhost:5000";
+const PRODUCTS_URL = "/api/products";
+const USERS_URL = "/api/users";
+const ORDERS_URL = "/api/orders";
+const PAYPAL_URL = "/api/config/paypal";
